3ce1d66dd91ebf052dceead766113a6c0e748555
Wenson Tsai <wensontsai@gmail.com>
2016-03-15 17:14:59 -0400

    progress

--- src/core.js
+++ src/core.js
@@ -1,5 +1,7 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries){
 	return state.set('entries', List(entries) );
 }
@@ -34,9 +36,9 @@ export function next(state){
 	});
 }
 
-export function vote(state, entry) {
-  return state.updateIn(
-    ['vote', 'tally', entry],
+export function vote(voteState, entry) {
+  return voteState.updateIn(
+    ['tally', entry],
     0,
     tally => tally + 1
   );
