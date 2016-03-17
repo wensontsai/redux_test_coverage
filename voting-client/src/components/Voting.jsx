1967b7cd10acbc458cf7e9be6586789d08388df3
Wenson Tsai <wensontsai@gmail.com>
2016-03-17 12:44:34 -0400

    progress on tests

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
-	getPair: function() {
-		return this.props.pair || [];
-	},
-	render: function() {
-		return (
-			<div>
-        {this.getPair().map(entry =>
-          <button key={entry}>
-            <h1>{entry}</h1>
-          </button>
-        )}
-			</div>
+  render: function() {
+    return (
+      <div>
+        {this.props.winner ? 
+          <Winner ref="winner" winner={this.props.winner} /> :
+          <Vote {...this.props} />
+        }
+      </div>
     )
-	}
+  }
 });
